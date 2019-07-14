491b3251088957d722553c1a9e5b004de87d8523
three4c <three4c@gmail.com>
2019-07-15 03:57:05 +0900

    電卓を作ってたがもっとシンプルにした

--- src/components/Result.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Result = () => (
-    <div>
-        Result: <span>some value</span>
-    </div>
-);
-
-export default Result;
--- src/ultils/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const INPUT_NUMBER = 'INPUT_NUMBER';
-export const PLUS = 'PLUS';
